e4f4e944f65a598835a85f94efd9455641ec9aaf
raphaelaloi <raphael@salestrekker.com>
2021-09-25 15:45:13 -0300

    Deployment

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './lib/redux';
+
+import InboxScreen, { PureInboxScreen } from './components/InboxScreen';
+
+import './index.css';
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <InboxScreen />
+    </Provider>
+  );
+}
+
+export default App;
